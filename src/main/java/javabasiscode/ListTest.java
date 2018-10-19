f7305960fa4632ed72ea6fe2aecc6ef6ecea385a
xuanjian <xuanjian@wacai.com>
2018-10-19 17:57:49 +0800

    update format

--- src/main/java/javabasiscode/ListTest.java
+++ src/main/java/javabasiscode/ListTest.java
@@ -1,4 +1,6 @@
-package javabasiscode;import java.util.ArrayList;
+package javabasiscode;
+
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Vector;
